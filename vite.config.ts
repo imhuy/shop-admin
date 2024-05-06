9a24ece899fa1e5c6b3cb8ef3229e64bd77824cd
Andy <huylv.dev@gmail.com>
2024-05-07 00:41:57 +0700

    first commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,11 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/admin/",
-  build: {
-    chunkSizeWarningLimit: 3000,
-  },
+  // base: "/admin/ ",
+  // build: {
+  //   chunkSizeWarningLimit: 3000,
+  // },
 });
